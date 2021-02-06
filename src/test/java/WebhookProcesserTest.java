161679f254a94e6826bed6e77ba2aa741a264b83
Adam Jonsson <adamsmajl@gmail.com>
2021-02-06 12:38:18 +0100

    Add CI server skeleton Remove HelloWorld project

---
+++ src/test/java/WebhookProcesserTest.java
@@ -0,0 +1,10 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class WebhookProcesserTest {
+    @Test
+    void dummyTest() {
+        assertTrue(true);
+    }
+}
