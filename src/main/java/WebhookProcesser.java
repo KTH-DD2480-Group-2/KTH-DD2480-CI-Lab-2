44ccb7345a39b21e67effa10101e9e61157b6526
manjikian <51725316+manjikian@users.noreply.github.com>
2021-02-08 19:03:28 +0100

    #7 feat: Make the CI server fetch data from the repository (#9)
    * Add CI server skeleton
    Remove HelloWorld project
    
    * Add quotes in maven run command
    This allows the command to also be used in powershell
    
    * Clean README
    
    * Update build banner for updated project
    
    * New try fixing build banner
    
    * Add tests to the ContinuousIntegrationServer class skeleton
    
    * Modify .gitignore and add .iml file
    
    * Modify .gitignore and add .iml file
    
    * Mofify .gitignore and add .iml file
    
    * Parse the incoming webhookpayload into a json.
    
    * Fetch the revision as a zip file
    Modify pom to solve Travis problem
    
    * Update tests for ContinuousIntegrationServer
    
    * Add tests to the file fetching process
    fix #7
    
    * Modify .gitignore and add .iml and .zip files
    
    * Remove .iml file
    
    Co-authored-by: Adam Jonsson <adamsmajl@gmail.com>
    Co-authored-by: Hovig <hovig@mail.com>

--- src/main/java/WebhookProcesser.java
+++ src/main/java/WebhookProcesser.java
@@ -1,18 +1,67 @@
 import javax.servlet.http.HttpServletRequest;
 
+import java.io.*;
+import java.net.URL;
+
+import org.json.JSONObject;
+
 public class WebhookProcesser {
 
-    private WebhookProcesser() {}
+    private WebhookProcesser() {
+    }
 
     /**
      * Handles a webhook request and runs maven tests and build the project
      * depending on the content in the request.
-     * @param request The request data
+     *
+     * @param request The request data.
      */
-    public static void handleWebhookEvent(HttpServletRequest request) {
-        // Here you do all the continuous integration tasks.
-        // For example:
-        // 1st clone your repository
-        // 2nd compile the code
+    public static void handleWebhookEvent(HttpServletRequest request) throws IOException {
+
+        JSONObject json = payloadToJSON(request);
+
+        String commitSHA = json.get("after").toString();
+
+        downloadRevision(commitSHA);
+    }
+
+    /**
+     * Will extract and convert the payload to a json file.
+     *
+     * @param request the request that contains the payload.
+     * @return a JSON object representing the payload.
+     * @throws UnsupportedEncodingException
+     */
+    private static JSONObject payloadToJSON(HttpServletRequest request) throws IOException {
+        StringBuilder builder = new StringBuilder();
+        String aux = "";
+        request.setCharacterEncoding("utf-8");
+
+        while ((aux = request.getReader().readLine()) != null) {
+            builder.append(aux);
+        }
+
+        return new JSONObject(builder.toString());
+    }
+
+    /**
+     * Downloads a revision in a single zip file.
+     *
+     * @param commitSHA the SHA-code of the revision.
+     */
+    private static void downloadRevision(String commitSHA) {
+        String revisionLink = "https://github.com/KTH-DD2480-Group-2/KTH-DD2480-CI-Lab-2/archive/" + commitSHA + ".zip";
+        try (
+                BufferedInputStream inputStream = new BufferedInputStream(new URL(revisionLink).openStream());
+                FileOutputStream fileOS = new FileOutputStream("revision.zip")
+        ) {
+            byte data[] = new byte[1024];
+            int byteContent;
+            while ((byteContent = inputStream.read(data, 0, 1024)) != -1) {
+                fileOS.write(data, 0, byteContent);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/test/java/ContinuousIntegrationServerTest.java
+++ src/test/java/ContinuousIntegrationServerTest.java
@@ -1,8 +1,16 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.FileEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.*;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -33,6 +41,26 @@ public class ContinuousIntegrationServerTest {
     }
 
     /**
+     * A function for sending an HTTP post request that contain a valid and real payload.
+     *
+     * @param url destination address
+     * @return response as a String
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    private static void postWithPayload(String url, String payloadFile) throws IOException, InterruptedException {
+        File file = new File(payloadFile);
+        FileEntity entity = new FileEntity(file, ContentType.create("text/plain", "UTF-8"));
+
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost(url);
+        httpPost.setEntity(entity);
+        CloseableHttpResponse response2 = httpclient.execute(httpPost);
+        response2.close();
+        httpclient.close();
+    }
+
+    /**
      * A class for making the StartServer function executable on a separate
      * thread during tests.
      */
@@ -47,6 +75,9 @@ public class ContinuousIntegrationServerTest {
         }
     }
 
+    /**
+     * Starts the server in a separate thread.
+     */
     @BeforeEach
     public void init() {
         RunnableServer server = new RunnableServer();
@@ -54,12 +85,69 @@ public class ContinuousIntegrationServerTest {
         t.start();
     }
 
+
+    /**
+     * Deletes downloaded files.
+     */
+    @AfterEach
+    public void fin() {
+        File file = new File("revision.zip");
+
+        if (file.delete()) {
+            System.out.println("File deleted successfully");
+        } else {
+            System.out.println("Failed to delete the file");
+        }
+    }
+
+    /**
+     * Tests weather the server is up and responding.
+     *
+     * @throws IOException
+     * @throws InterruptedException
+     */
     @Test
-    void main_ValidInput() throws IOException, InterruptedException {
+    void main_ValidInput_CheckServerStatus() throws IOException, InterruptedException {
         assertEquals("CI server working!\n", post("http://127.0.0.1:8080"));
         assertNotEquals("CI server working!", post("http://127.0.0.1:8080"));
-        assertEquals("Handling webhook event\n", post("http://127.0.0.1:8080/api/webhook-processer"));
-        assertNotEquals("Handling webhook vent\n", post("http://127.0.0.1:8080/api/webhook-processer"));
+    }
+
+    /**
+     * Tests with an invalid request
+     *
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    @Test
+    void main_InvalidInput_HandlingInvalidRequests() throws IOException, InterruptedException {
         assertEquals("404. Page not found\n", post("http://127.0.0.1:8080/xyz"));
+    }
+
+    /**
+     * Test webhook handling with valid payload.
+     *
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    @Test
+    void main_ValidInput_HandlingValidWebhook() throws IOException, InterruptedException {
+        postWithPayload("http://127.0.0.1:8080/api/webhook-processer","src/test/java/ValidGitPayload.txt");
+        File file = new File("revision.zip");
+        assertTrue(file.exists());
+        assertTrue(file.delete());
+    }
+
+    /**
+     * Test webhook handling with invalid payload.
+     *
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    @Test
+    void main_InvalidInput_HandlingValidWebhook() throws IOException, InterruptedException {
+        postWithPayload("http://127.0.0.1:8080/api/webhook-processer","src/test/java/InvalidGitPayload.txt");
+        File file = new File("revision.zip");
+        assertFalse(file.exists());
+        assertFalse(file.delete());
     }
 }
