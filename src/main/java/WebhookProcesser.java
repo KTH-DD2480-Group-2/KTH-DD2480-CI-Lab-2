e7ab65d6ee5cf44de56822ec14041ba1519036b9
Hovig <hovig@mail.com>
2021-02-08 10:51:49 +0100

    Fetch the revision as a zip file Modify pom to solve Travis problem

--- src/main/java/WebhookProcesser.java
+++ src/main/java/WebhookProcesser.java
@@ -1,14 +1,21 @@
 import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
 import org.json.JSONObject;
 
 public class WebhookProcesser {
 
-    private WebhookProcesser() {}
+    private WebhookProcesser() {
+    }
 
     /**
      * Handles a webhook request and runs maven tests and build the project
      * depending on the content in the request.
+     *
      * @param request The request data
      */
     public static void handleWebhookEvent(HttpServletRequest request) throws IOException {
@@ -20,11 +27,22 @@ public class WebhookProcesser {
             builder.append(aux);
         }
         JSONObject json = new JSONObject(builder.toString());
-        System.out.println(builder.toString());
-        System.out.println(json.get("zen").toString());
-        // Here you do all the continuous integration tasks.
-        // For example:
-        // 1st clone your repository
-        // 2nd compile the code
+
+        String commitSHA = json.get("after").toString();
+        String revisionLink = "https://github.com/KTH-DD2480-Group-2/KTH-DD2480-CI-Lab-2/archive/" + commitSHA + ".zip";
+
+
+        try (
+                BufferedInputStream inputStream = new BufferedInputStream(new URL(revisionLink).openStream());
+                FileOutputStream fileOS = new FileOutputStream("revision.zip")
+        ) {
+            byte data[] = new byte[1024];
+            int byteContent;
+            while ((byteContent = inputStream.read(data, 0, 1024)) != -1) {
+                fileOS.write(data, 0, byteContent);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
